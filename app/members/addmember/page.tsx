53a1ece5905af9bc44debd9f75f0ab425dc94460
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-20 00:20:10 +0300

    added 'addmember' page

---
+++ app/members/addmember/page.tsx
@@ -0,0 +1,12 @@
+import BasicInformationForm from '@/components/addmember/BasicInfo'
+import React from 'react'
+
+const addmember = () => {
+  return (
+    <div>
+      <BasicInformationForm />
+    </div>
+  )
+}
+
+export default addmember
