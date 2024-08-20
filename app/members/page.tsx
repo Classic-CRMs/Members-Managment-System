7d0d9e63742ab87b4976d09288446689b33a0979
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-21 00:20:45 +0300

    Component created for members list

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,8 +1,13 @@
+
+import MembersList from '@/components/memberslist/MembersList'
 import React from 'react'
 
 const members = () => {
   return (
-    <div>members</div>
+    <div className='max-w-4xl mx-auto'>
+      <h1 className='m-6 p-9 font-bold text-2xl'>Members List</h1>
+      <MembersList/>
+    </div>
   )
 }
 
