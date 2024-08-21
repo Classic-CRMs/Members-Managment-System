351d83e37cca4155b170b43907ef940252888e24
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-21 15:36:09 +0300

    db recursive connection fixed

--- config/db.ts
+++ config/db.ts
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
 
-// let isConnected = false;
+let isConnected = false;
 
 export const dbConnect = async () => {
-  //   if (isConnected) {
-  //     console.log('Already connected to MongoDB');
-  //     return;
-  //   }
+  if (isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
 
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI!, {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
 
-    // isConnected = db.connections[0].readyState
+    isConnected =
+      db.connections[0].readyState === mongoose.ConnectionStates.connected;
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
   }
 };
+
+
+export default dbConnect;
