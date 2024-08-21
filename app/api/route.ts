d5a9565328a5dbcd6744890ca8e8dc1a71038982
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-21 16:14:31 +0300

    API home route error handled

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const endpoints = {
-    "GET:/api/members": "List all members",
-    "GET:/api/members/:id": "Get a member by ID",
-    "POST:/api/members/add": "Create a new member",
-  };
-  return NextResponse.json(endpoints);
+  try {
+    const endpoints = {
+      "GET:/api/members": "List all members",
+      "GET:/api/members/:id": "Get a member by ID",
+      "POST:/api/members/add": "Create a new member",
+    };
+    return NextResponse.json(endpoints);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
 }
