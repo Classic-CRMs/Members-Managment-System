a1281faf76a4e78e56ced4029605fc7ef52cfd9c
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-21 16:21:23 +0300

    new member error handling added

--- app/api/members/new/route.ts
+++ app/api/members/new/route.ts
@@ -1,11 +1,15 @@
-import dbConnect from "@/config/db";
 import { NextResponse } from "next/server";
 import Member from "@/models/member";
-
+import dbConnect from "@/config/db";
 export async function POST(req: Request) {
-  const newMember = await req.json();
-  const newMemberObject = new Member(newMember);
-  await dbConnect();
-  await newMemberObject.save();
-  return NextResponse.json(newMember);
+  try {
+    const newMember = await req.json();
+    const newMemberObject = new Member(newMember);
+    await dbConnect();
+    await newMemberObject.save();
+    return NextResponse.json(newMember);
+  } catch (error) {
+    console.error("Error saving new member:", error);
+    return NextResponse.error();
+  }
 }
