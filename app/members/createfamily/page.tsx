5417e6a94bc3b3651a39c9d54b404b3c8e381833
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-22 15:33:22 +0300

    added create family

--- app/members/createfamily/page.tsx
+++ app/members/createfamily/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import FamilyForm from "@/components/createfamily/FamilyForm";
+import React from "react";
 
 const CreateFamily = () => {
   return (
-    <div>CreateFamily</div>
-  )
-}
+    <div>
+      <h1 className="flex justify-start m-6 p-9 font-bold text-2xl">
+        Add new child
+      </h1>
+      <FamilyForm />
+    </div>
+  );
+};
 
-export default CreateFamily
+export default CreateFamily;
--- components/createfamily/FamilyForm.tsx
+++ components/createfamily/FamilyForm.tsx
@@ -122,7 +122,7 @@ const FamilyForm: React.FC = () => {
                   <svg className="mx-auto h-12 w-12 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                   </svg>
-                  <p className="mt-1">Drag 'n' drop an image here, or click to select</p>
+                  <p className="mt-1">Drag n drop an image here, or click to select</p>
                 </div>
               )}
             </div>
