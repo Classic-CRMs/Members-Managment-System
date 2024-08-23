70f4dd6ffac901c8e97226a801e25ba5309203de
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-23 13:22:42 +0300

    side nav style updated

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import Image from 'next/image';
-import { FaChevronRight } from 'react-icons/fa';
-import Link from 'next/link';
-import ActiveLink from '@/utils/ActiveLink';
+import React from "react";
+import Image from "next/image";
+import { FaChevronDown, FaChevronRight } from "react-icons/fa";
+import Link from "next/link";
 
 interface ListItemProps {
+  href: string;
   icon: string;
   label: string;
-  href: any;
   hasChevron?: boolean;
+  isActive?: boolean;
+  isChevOpen?: boolean;
 }
 
-const ListItem: React.FC<ListItemProps> = ({ icon, label, href,hasChevron = false }) => {
+const ListItem: React.FC<ListItemProps> = ({
+  icon,
+  label,
+  href,
+  hasChevron = false,
+  isActive = false,
+  isChevOpen = false,
+}) => {
   return (
-    <ActiveLink href={href} className="py-2 px-4 flex items-center text-gray-600 hover:bg-gray-100">
+    <Link
+      href={href}
+      className={`py-2 px-4 flex items-center text-gray-600 rounded-lg hover:bg-purple-50 ${
+        isActive && "bg-[#5932EA] hover:bg-[#5932EA] text-white"
+      }`}
+    >
       <span className="w-5 h-5 mr-3 flex items-center justify-center">
-        <Image src={`/${icon}.svg`} width={20} height={20} alt={label} />
+        <Image
+          src={isActive ? `/white/${icon}.svg` : `/${icon}.svg`}
+          width={20}
+          height={20}
+          alt={label}
+        />
       </span>
       <span className="flex-1">{label}</span>
-      {hasChevron && <FaChevronRight size={12} className="text-gray-400" />}
-    </ActiveLink>
+      {hasChevron && (isChevOpen ? (
+        <FaChevronDown size={12} />
+      ) :  (
+        <FaChevronRight size={12} />
+      ))}
+    </Link>
   );
 };
 
