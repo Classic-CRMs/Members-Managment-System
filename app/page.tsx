fdc7002aa465295fe79c6618a22322a1b036ae82
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-23 13:21:38 +0300

    added stats to main dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Stats from "@/components/StatsComponents/Stats";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <>Dashboard</>
+   <div className="max-w-4xl mx-auto">
+    <h1 className="flex justify-start m-6 font-bold text-2xl">
+        Dashboard
+    </h1>
+    <Stats/>
+   </div>
   );
 }
