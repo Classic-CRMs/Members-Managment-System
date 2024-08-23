c2c38ac062143143c8411816a5212b3db0c9d959
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-23 13:23:35 +0300

    modified heading

--- app/members/createfamily/page.tsx
+++ app/members/createfamily/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const CreateFamily = () => {
   return (
     <div>
-      <h1 className="flex justify-start m-6 p-9 font-bold text-2xl">
-        Add new child
+      <h1 className="flex justify-start ml-28 my-14 font-bold text-2xl">
+        Create Family
       </h1>
       <FamilyForm />
     </div>
--- app/members/listfamily/page.tsx
+++ app/members/listfamily/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const FamilyList = () => {
   return (
-    <div>FamilyList</div>
+    <div>
+      <h1 className="flex justify-start ml-28 my-14 font-bold text-2xl">
+        Families
+      </h1>
+    </div>
   )
 }
 
