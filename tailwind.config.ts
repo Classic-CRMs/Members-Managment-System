a2df4e632b7874881d590a509ce3f9ef3abc1f50
Halleluya Mulugeta <halleluya2013idone@gmail.com>
2024-08-23 13:18:24 +0300

    added font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      "fontFamily": {
+        "poppins": "Poppins",
+        "nunito-sans": "'Nunito Sans'",
+        "inter": "Inter"
+      },
     },
   },
   plugins: [],
