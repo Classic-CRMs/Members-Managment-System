cb806c4b2656440bb7b6377635c70bd36d77dd05
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-25 00:26:16 +0300

    family schema updated

--- models/family.ts
+++ models/family.ts
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const FamilySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  parents: {
-    type: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Member",
+const FamilySchema = new mongoose.Schema(
+  {
+    familyName: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    members: [{
+      _id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true,
+        refPath: 'members.memberType'
       },
-    ],
-  },
-  children: {
-    type: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Child",
+      memberType: {
+        type: String,
+        required: true,
+        enum: ['Member', 'Child']
       },
-    ],
+      isChild: { type: Boolean, required: true }
+    }]
   },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Family =
   mongoose.models.Family || mongoose.model("Family", FamilySchema, "Family");
-
 
 export default Family;
