1348edb9f37dfc2fd2040f80e296fa69eee1cbcd
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-26 01:37:36 +0300

    adding child to family collection with validation

--- app/api/children/new/route.ts
+++ app/api/children/new/route.ts
@@ -3,46 +3,52 @@ import Child from "@/models/child";
 import dbConnect from "@/config/db";
 import mongoose from "mongoose";
 import Family from "@/models/family";
+
 export async function POST(req: Request) {
   try {
-    const newChild = await req.json();
-    if (!mongoose.Types.ObjectId.isValid(newChild.family_id)) {
-      return NextResponse.json({ error: "Family ID Invalid" }, { status: 400 });
+    await dbConnect();
+
+    const newChildData = await req.json();
+
+    if (!mongoose.Types.ObjectId.isValid(newChildData.family_id)) {
+      return NextResponse.json({ error: "Invalid Family ID" }, { status: 400 });
     }
-    const familyId = newChild.family_id;
-    const family = await Family.findByIdAndUpdate(
-      { _id: familyId },
-      { $push: { members: { _id: newChild._id, isChild: true } } }
-    );
-    if (!family) {
-      return NextResponse.json({ error: "Family Not Found" }, { status: 404 });
-    }
-    const newChildObject = new Child(newChild);
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
     try {
-      await dbConnect();
-    } catch (dbError) {
-      console.error("Database Connection Error:", dbError);
+      const newChildObject = new Child(newChildData);
+      const savedChild = await newChildObject.save({ session });
+
+      const familyId = newChildData.family_id;
+      const family = await Family.findByIdAndUpdate(
+        familyId,
+        { $push: { members: { _id: savedChild._id, isChild: true } } },
+        { new: true, session }
+      );
+
+      if (!family) {
+        await session.abortTransaction();
+        return NextResponse.json(
+          { error: "Family Not Found" },
+          { status: 404 }
+        );
+      }
+
+      await session.commitTransaction();
+
+      return NextResponse.json(savedChild);
+    } catch (error: unknown) {
+      await session.abortTransaction();
+      console.error("Error During Transaction:", error);
       return NextResponse.json(
-        {
-          error:
-            "Database Connection Error: Unable to connect to the database.",
-        },
+        { error: "Error During Transaction: Unable to process the request." },
         { status: 500 }
       );
+    } finally {
+      session.endSession();
     }
-
-    try {
-      await newChildObject.save();
-    } catch (saveError) {
-      console.error("Database Save Error:", saveError);
-      return NextResponse.json(
-        { error: "Database Save Error: Unable to save the new member." },
-        { status: 500 }
-      );
-    }
-
-    return NextResponse.json(newChild);
   } catch (error) {
     console.error("Unexpected Error:", error);
     return NextResponse.json(
